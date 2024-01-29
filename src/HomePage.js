412124f9843c6b1b48ac4bb95aedde14134793a2
connorq <connorquitt@gmail.com>
2024-01-29 15:02:50 -0600

    fixed json, got post working

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,30 +1,60 @@
 import React from "react";
 import { useState } from "react";
 import './App.css';
-import { NavLink } from "react-router-dom";
 import NavBar from "./navbar";
+import MovieCard from "./MovieCard";
 
-function HomePage({ isLoggedIn, setIsLoggedIn }) {
-    const [username, setUsername] = useState('')
+function HomePage() {
 
-        function HandleChange(e) {
-            setUsername(e.target.value)
-        }
+    const [newMovie, setNewMovie] = useState([])
 
-        function HandleClick(e) {
-            console.log(e)
-        }
+    function MakeMovieCard(){
+       return (
+        <div className="Movie-Maker">
+            Title: <input type='text' id='title'/>
+            Genre: <input type='text' id='genre'/>
+            Length: <input type='text' id='length'/>
+            Rating <input type='text' id='rating'/>
+            <button onClick={handleClick}>Submit Movie</button>
+        </div>
+       )
+    }
+
+    function handleClick(e) {
+        const title = document.getElementById('title').value
+        const genre = document.getElementById('genre').value
+        const length = document.getElementById('length').value
+        const rating = document.getElementById('rating').value
+        setNewMovie([{
+            title: title,
+            genres: genre,
+            director: 'NA',
+            leads: 'NA',
+            length: length,
+            rating: rating,
+            reviews: 'NA'
+        }]) 
+        return fetch('http://localhost:3000/Movies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newMovie)
+        })
+        .then(res => res.json())
+    }
 
     return (
         <div className="HomePage">
             <header className="HomeHeader">
                 <NavBar />
-                <h1 >Movie Reviewer</h1>
+                <h1 >Movie Notes</h1>
             </header>
+            <MakeMovieCard />
         </div>
     )
 }
 
 export default HomePage;
 
-//make sure to make 'check state' button clears the login bar and makes the pfp + username show up on the top and update the isLoggedIn state
+// did this shit wrong, gonna make the homepage the spot to like add movies with a lil guy that lets you add the name length and rating and maybe a review or smthn but idk for sure
